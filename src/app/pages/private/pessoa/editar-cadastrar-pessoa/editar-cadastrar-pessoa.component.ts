b7363000dc37579c21e456cd2bdc187884076ee6
Jhonathan Baldin <jhonathan@d7app.com.br>
2021-06-01 20:23:52 -0300

    Pessoa

--- src/app/pages/private/pessoa/editar-cadastrar-pessoa/editar-cadastrar-pessoa.component.ts
+++ src/app/pages/private/pessoa/editar-cadastrar-pessoa/editar-cadastrar-pessoa.component.ts
@@ -6,6 +6,8 @@ import {PessoasService} from "../../../../services/pessoas.service";
 import * as moment from "moment";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {ActivatedRoute, Router} from "@angular/router";
+import { Funcionario } from '../../../../models/funcionario.model';
+import { Paciente } from '../../../../models/paciente.model';
 
 @Component({
   selector: 'app-editar-cadastrar-pessoa',
@@ -15,6 +17,8 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class EditarCadastrarPessoaComponent implements OnInit {
   public masks = new Masks();
   public pessoa = new Pessoa();
+  public funcionario = new Funcionario();
+  public paciente = new Paciente();
   public form: FormGroup;
   public novaPessoa = false;
   public jaSalvo = false;
@@ -27,9 +31,10 @@ export class EditarCadastrarPessoaComponent implements OnInit {
     this.activeRoute.params.subscribe(params => {
       this.novaPessoa = params.id === 'new';
       if (!this.novaPessoa) {
+        this.jaSalvo = true;
         this.getById(params.id);
       } else {
-        this.jaSalvo = true;
+
       }
     });
   }
@@ -60,6 +65,24 @@ export class EditarCadastrarPessoaComponent implements OnInit {
           duration: 3000
         });
         this.router.navigate(['/pessoas']);
+      }, error => {
+        this.snackBar.open('Erro durante o processamento!', 'X', {
+          duration: 3000
+        });
+      });
+    }
+  }
+
+
+  onSubmitFuncionario() {
+    if (this.novaPessoa) {
+
+      this.pessoaService.save(this.pessoa).subscribe(res => {
+        this.snackBar.open('Pessoa cadastrada com sucesso!', 'X', {
+          duration: 3000
+        });
+        this.jaSalvo = true;
+        // this.router.navigate(['/pessoas']);
       }, error => {
         this.snackBar.open('Erro durante o processamento!', 'X', {
           duration: 3000
